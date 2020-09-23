43fae298b32e48b91e20faa1a61c85f1755dc984
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-23 12:04:42 +0530

    [Ashutosh]Added:UC6 Push

--- src/empcode/EmpCheck.java
+++ src/empcode/EmpCheck.java
@@ -6,15 +6,15 @@ public class EmpCheck {
 	public static final int Is_Full_Time=2;
 	public static final int Emp_Rate_Per_Hour=20;
 	public static final int Num_Of_Working_Days=20;
-	
+	public static final int Max_Hrs_Per_Month=100;
 	public static void main(String[] args) {
-				
-			int empHrs=0;
-			int empWage=0;
-			int totalEmpWage=0;
+		
 			
-			for(int day=0;day<Num_Of_Working_Days;day++)
+			int empHrs=0, totalEmpHrs=0, totalWorkingDays=0;
+			
+			while(totalEmpHrs<=Max_Hrs_Per_Month && totalWorkingDays<Num_Of_Working_Days)
 			{
+				totalWorkingDays++;
 				int empCheck=(int)Math.floor(Math.random()*10)%3;
 				switch(empCheck) {
 				case Is_Part_Time:
@@ -28,15 +28,12 @@ public class EmpCheck {
 					default:
 						empHrs=0;
 			}
-			empWage=empHrs*Emp_Rate_Per_Hour;
-			totalEmpWage+=empWage;
-			System.out.println("Emp Wage : "+empWage);
-				
-				
+				totalEmpHrs+=empHrs;
+				System.out.println("Day#: "+totalWorkingDays+" EmpHrs: "+empHrs);
 			}
+			int totalEmpWage=totalEmpHrs*Emp_Rate_Per_Hour;
 			System.out.println("Total Emp Wage : "+totalEmpWage);
 			
-
 
 	}
 
