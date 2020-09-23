ff9145eab96ecebbd007cb69a2e42d80fdf2bb80
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-23 11:51:42 +0530

    [Ashutosh]Added:UC2 Push

--- src/empcode/EmpCheck.java
+++ src/empcode/EmpCheck.java
@@ -2,14 +2,18 @@ package empcode;
 
 public class EmpCheck {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+		
 		int Is_Full_Time=1;
+		int Emp_Rate_Per_hour=20;
+		int empHrs=0;
+		int empWage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==Is_Full_Time)
-			System.out.println("Employee Present");
+			empHrs=8;
 		else
-			System.out.println("Employee Absent");
-			
-
+			empHrs=0;
+		empWage= empHrs * Emp_Rate_Per_hour;
+		System.out.println("Emp Wage : "+empWage);
 	}
 }
