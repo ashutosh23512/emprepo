7fed8efa473f0e0eb31f5afd9f9d61f63e7b7be3
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-23 11:47:19 +0530

    [Ashutosh]Added:UC1 Push

--- src/empcode/EmpCheck.java
+++ src/empcode/EmpCheck.java
@@ -2,4 +2,14 @@ package empcode;
 
 public class EmpCheck {
 
+	public static void main(String[] args) {
+		int Is_Full_Time=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==Is_Full_Time)
+			System.out.println("Employee Present");
+		else
+			System.out.println("Employee Absent");
+			
+
+	}
 }
